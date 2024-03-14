da386d802f13444ab94b6c2b15c628c02fc16742
Azizaazu <aynaou.aziza98@gmail.com>
2024-03-14 04:06:44 -0700

    update

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -2,12 +2,16 @@
 """ method that determines if all the boxes can be opened."""
 
 def canUnlockAll(boxes):
-    """Determines if boxes can be unlocked"""
-    if not boxes:
+    """Determines if boxes can be unlocked
+    boxes: prameter
+    return: True or False
+
+    """
+    if not boxes or type(boxes) is not list:
         return False
 
     visited = set()
-    queue = [0]  # Start with the first box (box 0)
+    queue = [0]
 
     while queue:
         current_box = queue.pop(0)
@@ -17,4 +21,6 @@ def canUnlockAll(boxes):
             if key not in visited and key < len(boxes):
                 queue.append(key)
 
-    return len(visited) == len(boxes)
+    if len(visited) == len(boxes):
+        return True
+    return False
