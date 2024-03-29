825729146cf2f915c028279da1404fac72b9a784
Azizaazu <aynaou.aziza98@gmail.com>
2024-03-28 17:03:09 -0700

    update

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -7,19 +7,19 @@ reads stdin line by line and computes metrics
 import sys
 
 
-status_counts = {
-    200: 0,
-    301: 0,
-    400: 0,
-    401: 0,
-    403: 0,
-    404: 0,
-    405: 0,
-    500: 0
+cache = {
+    '200': 0,
+    '301': 0,
+    '400': 0,
+    '401': 0,
+    '403': 0,
+    '404': 0,
+    '405': 0,
+    '500': 0
 }
 
-total_file_size = 0
-line_count = 0
+total_size = 0
+counter = 0
 
 
 try:
@@ -28,15 +28,15 @@ try:
         if len(line_list) > 4:
             code = line_list[-2]
             size = int(line_list[-1])
-            if code in status_counts.keys():
-                status_counts[code] += 1
-            total_file_size += size
-            line_count += 1
+            if code in cache.keys():
+                cache[code] += 1
+            total_size += size
+            counter += 1
 
-        if line_count == 10:
-            line_count = 0
-            print('File size: {}'.format(total_file_size))
-            for key, value in sorted(status_counts.items()):
+        if counter == 10:
+            counter = 0
+            print('File size: {}'.format(total_size))
+            for key, value in sorted(cache.items()):
                 if value != 0:
                     print('{}: {}'.format(key, value))
 
@@ -44,7 +44,7 @@ except Exception as err:
     pass
 
 finally:
-    print('File size: {}'.format(total_file_size))
-    for key, value in sorted(status_counts.items()):
+    print('File size: {}'.format(total_size))
+    for key, value in sorted(cache.items()):
         if value != 0:
             print('{}: {}'.format(key, value))
