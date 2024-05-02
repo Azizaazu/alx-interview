de304e2ff98672af4b47efd18c13368932988a06
Azizaazu <aynaou.aziza98@gmail.com>
2024-05-02 22:40:52 +0000

    update

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 def makeChange(coins, total):
+    """ Determines the fewest number of coins
+    """
     if total <= 0:
         return 0
 
@@ -12,7 +14,3 @@ def makeChange(coins, total):
             dp[i] = min(dp[i], dp[i - coin] + 1)
 
     return dp[total] if dp[total] != float('inf') else -1
-
-if __name__ == "__main__":
-    print(makeChange([1, 2, 25], 37))
-    print(makeChange([1256, 54, 48, 16, 102], 1453))
