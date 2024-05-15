633d66cb67e3f01d5bc311e8617e4ea635ff8669
Azizaazu <aynaou.aziza98@gmail.com>
2024-05-15 19:21:27 +0000

    update

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -3,7 +3,7 @@
 
 
 def is_prime(n):
-    """ Determines the winner
+    """ Checks if a number is prime.
     """
     if n <= 1:
         return False
@@ -18,28 +18,22 @@ def is_prime(n):
         i += 6
     return True
 
-def prime_count(n):
-    primes = [0] * (n + 1)
-    count = 0
-    for i in range(2, n + 1):
-        if primes[i] == 0 and is_prime(i):
-            count += 1
-            for j in range(i, n + 1, i):
-                primes[j] = 1
-    return count
-
 def isWinner(x, nums):
-    wins_maria = 0
-    wins_ben = 0
+    """
+    Determines the winner of each round of the prime game.
+    """
+    marias_wins = 0
     for n in nums:
-        prime_count_n = prime_count(n)
-        if prime_count_n % 2 == 0:
-            wins_ben += 1
-        else:
-            wins_maria += 1
-    if wins_maria == wins_ben:
-        return None
-    return "Maria" if wins_maria > wins_ben else "Ben"
+        prime_count = sum(1 for i in range(1, n + 1) if is_prime(i))
+        if prime_count % 2 == 0:
+            marias_wins += 1
 
-print("Winner:", isWinner(3, [4, 5, 1]))  # Output: Ben
-print("Winner:", isWinner(5, [2, 5, 1, 4, 3]))  # Output: Maria
+    if marias_wins > x // 2:
+        return "Ben"
+    elif marias_wins < x // 2:
+        return "Maria"
+    else:
+        return None
+
+if __name__ == "__main__":
+    print("Winner: {}".format(isWinner(5, [2, 5, 1, 4, 3])))
